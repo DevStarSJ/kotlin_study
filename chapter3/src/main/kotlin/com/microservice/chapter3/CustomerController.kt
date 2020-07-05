c2421795282614079d5ce6516697ef2364d3722c
seokjoon.yun <seokjoon.yun@gmail.com>
2020-07-05 16:20:26 +0900

    @RequestMapping -> @동사 + Mapping

--- chapter3/src/main/kotlin/com/microservice/chapter3/CustomerController.kt
+++ chapter3/src/main/kotlin/com/microservice/chapter3/CustomerController.kt
@@ -9,26 +9,26 @@ class CustomerController {
     @Autowired
     lateinit var customers: ConcurrentHashMap<Int, Customer>
 
-    @RequestMapping(value = ["/customers"], method= [RequestMethod.GET])
+    @GetMapping(value = ["/customers"])
     fun getCustomers(@RequestParam(required = false, defaultValue = "") nameFilter: String) =
         customers.filter {
             it.value.name.contains(nameFilter, true)
         }.map(Map.Entry<Int, Customer>::value).toList()
 
-    @RequestMapping(value = ["/customer/{id}"], method = [RequestMethod.GET])
+    @GetMapping(value = ["/customer/{id}"])
     fun getCustomer(@PathVariable id: Int) = customers[id]
 
-    @RequestMapping(value = ["/customer/", "/customer"], method = [RequestMethod.POST])
+    @PostMapping(value = ["/customer/", "/customer"])
     fun createCustomer(@RequestBody customer: Customer) {
         customers[customer.id] = customer
     }
 
-    @RequestMapping(value = ["/customer/{id}"], method = [RequestMethod.PUT])
+    @PutMapping(value = ["/customer/{id}"])
     fun updateCustomer(@PathVariable id: Int, @RequestBody customer: Customer) {
         deleteCustomer(id)
         createCustomer(customer)
     }
 
-    @RequestMapping(value = ["/customer/{id}"], method= [RequestMethod.DELETE])
+    @DeleteMapping(value = ["/customer/{id}"])
     fun deleteCustomer(@PathVariable id: Int) = customers.remove(id)
 }
