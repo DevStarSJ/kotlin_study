5d93a980c90492bc5470cb7414e1082a3a487164
seokjoon.yun <seokjoon.yun@gmail.com>
2020-07-05 11:12:39 +0900

    use @Bean instead of @Service

--- chapter2/src/main/kotlin/com/microservice/chapter2/ExampleService.kt
+++ chapter2/src/main/kotlin/com/microservice/chapter2/ExampleService.kt
@@ -3,7 +3,6 @@ package com.microservice.chapter2
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 
-@Service
 class ExampleService: ServiceInterface {
     @Value(value= "\${service.message.text}")
     private lateinit var text: String
