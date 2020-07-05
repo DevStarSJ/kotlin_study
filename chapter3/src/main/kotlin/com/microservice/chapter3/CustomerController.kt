72f093341d630b646bc61b16f260a5b81ff6b293
seokjoon.yun <seokjoon.yun@gmail.com>
2020-07-05 16:43:33 +0900

    Http Status

--- chapter3/src/main/kotlin/com/microservice/chapter3/CustomerController.kt
+++ chapter3/src/main/kotlin/com/microservice/chapter3/CustomerController.kt
@@ -1,6 +1,8 @@
 package com.microservice.chapter3
 
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 import java.util.concurrent.ConcurrentHashMap
 
@@ -13,14 +15,33 @@ class CustomerController {
     fun getCustomers(@RequestParam(required = false, defaultValue = "") nameFilter: String) = customerService.searchCustomers(nameFilter)
 
     @GetMapping(value = ["/customer/{id}"])
-    fun getCustomer(@PathVariable id: Int) = customerService.getCustomer(id)
+    fun getCustomer(@PathVariable id: Int): ResponseEntity<Customer> {
+        val customer = customerService.getCustomer(id)
+        val status = if (customer == null) HttpStatus.NOT_FOUND else HttpStatus.OK
+        return ResponseEntity(customer, status)
+    }
 
     @PostMapping(value = ["/customer/", "/customer"])
-    fun createCustomer(@RequestBody customer: Customer) = customerService.createCustomer(customer)
+    fun createCustomer(@RequestBody customer: Customer): ResponseEntity<Unit> =
+        ResponseEntity(customerService.createCustomer(customer), HttpStatus.CREATED)
 
     @PutMapping(value = ["/customer/{id}"])
-    fun updateCustomer(@PathVariable id: Int, @RequestBody customer: Customer) = customerService.updateCustomer(id, customer)
+    fun updateCustomer(@PathVariable id: Int, @RequestBody customer: Customer): ResponseEntity<Unit> {
+        var status = HttpStatus.NOT_FOUND
+        if (customerService.getCustomer(id) != null) {
+            customerService.updateCustomer(id, customer)
+            status = HttpStatus.ACCEPTED
+        }
+        return ResponseEntity(Unit, status)
+    }
 
     @DeleteMapping(value = ["/customer/{id}"])
-    fun deleteCustomer(@PathVariable id: Int) = customerService.deleteCustomer(id)
+    fun deleteCustomer(@PathVariable id: Int): ResponseEntity<Unit> {
+        var status = HttpStatus.NOT_FOUND
+        if (customerService.getCustomer(id) != null) {
+            customerService.deleteCustomer(id)
+            status = HttpStatus.OK
+        }
+        return ResponseEntity(Unit, status)
+    }
 }
