8e5b255e91277aaea2e3ba694c356ad446e91e0f
seokjoon.yun <seokjoon.yun@gmail.com>
2020-07-07 09:32:26 +0900

    add CustomerHandler for CustomerRouter

--- chapter4/src/main/kotlin/com/microservices/chapter4/CustomerRouter.kt
+++ chapter4/src/main/kotlin/com/microservices/chapter4/CustomerRouter.kt
@@ -1,21 +1,22 @@
 package com.microservices.chapter4
 
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Bean
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.server.RouterFunction
-import org.springframework.web.reactive.function.server.ServerResponse
+import org.springframework.web.reactive.function.server.ServerRequest
 import org.springframework.web.reactive.function.server.router
-import reactor.kotlin.core.publisher.toMono
 
 @Component
 class CustomerRouter {
+    @Autowired
+    lateinit var customerHandler: CustomerHandler
+
     @Bean
     fun customerRoutes(): RouterFunction<*> = router {
         "/functional".nest {
             "/customer".nest {
-                GET("/") {
-                    ServerResponse.ok().body(Customer(1, "functional web").toMono(), Customer::class.java)
-                }
+                GET("/") {it: ServerRequest -> customerHandler.get(it) }
             }
         }
     }
