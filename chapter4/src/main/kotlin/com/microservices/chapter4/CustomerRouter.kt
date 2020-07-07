f1876737c48549500709384f4b2926576f32633e
seokjoon.yun <seokjoon.yun@gmail.com>
2020-07-07 09:42:43 +0900

    refactor : use lambda. remove @Autowire

--- chapter4/src/main/kotlin/com/microservices/chapter4/CustomerRouter.kt
+++ chapter4/src/main/kotlin/com/microservices/chapter4/CustomerRouter.kt
@@ -1,22 +1,17 @@
 package com.microservices.chapter4
 
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Bean
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.server.RouterFunction
-import org.springframework.web.reactive.function.server.ServerRequest
 import org.springframework.web.reactive.function.server.router
 
 @Component
-class CustomerRouter {
-    @Autowired
-    lateinit var customerHandler: CustomerHandler
-
+class CustomerRouter(private val customerHandler: CustomerHandler) {
     @Bean
     fun customerRoutes(): RouterFunction<*> = router {
         "/functional".nest {
             "/customer".nest {
-                GET("/") {it: ServerRequest -> customerHandler.get(it) }
+                GET("/", customerHandler::get)
             }
         }
     }
