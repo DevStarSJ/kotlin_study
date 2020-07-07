e4fdd569b5a14e368f2af917e1425dfa69320e36
seokjoon.yun <seokjoon.yun@gmail.com>
2020-07-07 15:15:09 +0900

    error 처리 (but, default error가 더 좋음)

--- chapter4/src/main/kotlin/com/microservices/chapter4/CustomerHandler.kt
+++ chapter4/src/main/kotlin/com/microservices/chapter4/CustomerHandler.kt
@@ -7,6 +7,7 @@ import org.springframework.web.reactive.function.BodyInserters.fromObject
 import org.springframework.web.reactive.function.server.ServerRequest
 import org.springframework.web.reactive.function.server.ServerResponse.*
 import org.springframework.web.reactive.function.server.bodyToMono
+import reactor.kotlin.core.publisher.onErrorResume
 
 @Component
 class CustomerHandler(val customerService: CustomerService) {
@@ -17,5 +18,8 @@ class CustomerHandler(val customerService: CustomerService) {
     fun search(serverRequest: ServerRequest) = ok().body(customerService.searchCustomers(serverRequest.queryParam("nameFilter").orElse("")), Customer::class.java)
 
     fun create(serverRequest: ServerRequest) = customerService.createCustomer(serverRequest.bodyToMono()).flatMap {
-        created(java.net.URI.create("/functional/customer/${it.id}")).build() }
+            created(java.net.URI.create("/functional/customer/${it.id}")).build()
+        }.onErrorResume(Exception::class) {
+            badRequest().body(fromObject("error"))
+        }
 }
