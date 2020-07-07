6841da3b031f0adb18d508a2731a21b84acc74a8
seokjoon.yun <seokjoon.yun@gmail.com>
2020-07-07 09:57:29 +0900

    refactor : using service

--- chapter4/src/main/kotlin/com/microservices/chapter4/CustomerHandler.kt
+++ chapter4/src/main/kotlin/com/microservices/chapter4/CustomerHandler.kt
@@ -3,11 +3,10 @@ package com.microservices.chapter4
 
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.server.ServerRequest
-import org.springframework.web.reactive.function.server.ServerResponse
-import reactor.core.publisher.Mono
-import reactor.kotlin.core.publisher.toMono
+import org.springframework.web.reactive.function.server.ServerResponse.ok
+import org.springframework.web.reactive.function.server.body
 
 @Component
-class CustomerHandler {
-    fun get(serverRequest: ServerRequest): Mono<ServerResponse> = ServerResponse.ok().body(Customer(1, "functional web").toMono(), Customer::class.java)
+class CustomerHandler(val customerService: CustomerService) {
+    fun get(serverRequest: ServerRequest) = ok().body(customerService.getCustomer(1))
 }
